95941fa395c68f75f7b06827b3e9b9c5ba66198b
usama508 <musama023418@gmail.com>
2023-05-22 23:25:39 +0500

    navbar & hero component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-export default function App() {
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
   )
 }
+
+export default App
