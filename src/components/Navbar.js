7150c9381fbfd0499998f25d36aa4a35e813adba
usama508 <musama023418@gmail.com>
2023-05-24 00:19:29 +0500

    new changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
                     <AiOutlineMenu size={30} />
                 </div>
                 <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
-                    Best <span className='font-bold'>Eats</span>
+                    Pak <span className='font-bold'>Food</span>
                 </h1>
                 <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
                     <p className='bg-black text-white rounded-full p-2'>Delivery</p>
@@ -44,7 +44,7 @@ function Navbar() {
             <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'
             :'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'}>
                 <AiOutlineClose onClick={()=>setNav(!nav)} size={30} className='absolute right-4 top-4 cursor-pointer'/>
-                <h2 className='text-2xl p-4'>Best <span className='font-bold'>Eats</span>
+                <h2 className='text-2xl p-4'>Pak <span className='font-bold'>Food</span>
                 </h2>
                 <nav>
                     <ul className='flex flex-col p-4 text-gray-800'>
